63e3b29d5f48711b97cae856debacc351c6f2143
pramesh167 <prameshpathak2002@gmail.com>
2025-01-12 09:08:28 +0545

    indexjs configuration

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+
+const app = require('./app');
+
+const uri = process.env.MONGODB_URI;
+console.log('MongoDB URI:', uri); // Add this line to verify the URI is being loaded
+
+if (!uri) {
+  console.error('MongoDB URI is not defined. Check your .env file.');
+  process.exit(1);
+}
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database:', error.message);
+    process.exit(1);
+  });
+
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`Server is running on port ${port}`));
