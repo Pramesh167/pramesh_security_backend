ce6509c5cbc53eddd11e24d9e9cc531c360f8b5e
pramesh167 <prameshpathak2002@gmail.com>
2025-01-14 13:17:38 +0545

    api indexing of cart

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ app.get('/Robsell',(req,res)=>{
 
 app.use('/api/user', require('./routes/userRoutes'))
 app.use('/api/product', require('./routes/productRoutes'))
+app.use('/api/cart', require('./routes/cartRoutes'))
 
 
 
