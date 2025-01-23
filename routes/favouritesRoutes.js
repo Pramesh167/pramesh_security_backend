e8170276b095f418972192f0537801546e3bf2b9
pramesh167 <prameshpathak2002@gmail.com>
2025-01-23 17:53:16 +0545

    faviourite product deletion with secured user method access

--- routes/favouritesRoutes.js
+++ routes/favouritesRoutes.js
@@ -4,7 +4,7 @@ const { authGuard } = require('../middleware/authGuard');
 
 
 router.post('/add_favourite', authGuard, favoritesController.addFavorite);
-router.delete('/remove_favourite/:id', authGuard, favoritesController.removeFavorite);
+router.put('/remove_favourite/:id',  favoritesController.removeFavorite);
 router.get('/get_favourite', authGuard, favoritesController.getFavorites);
 
 module.exports = router;
